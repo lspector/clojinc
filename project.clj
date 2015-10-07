e84d35664f218f9947141d7ec6b08f3604a1372b
Tom Helmuth <thelmuth@cs.umass.edu>
2015-10-06 20:35:41 -0400

    Updated versions for Clojure and local-file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojinc "0.0.2"
   :description "Steps toward clojure, starting from zero. By Lee Spector. See comments 
                 in src/clojinc/core.clj."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [local-file "0.0.4"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [local-file "0.1.0"]])
